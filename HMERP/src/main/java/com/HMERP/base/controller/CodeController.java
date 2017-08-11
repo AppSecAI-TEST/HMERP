fd1b05b24c3f979b3d88823d07eabde74d17a967
KimMavi <KimMavi.tex8650@gmail.com>
2017-08-11 10:44:13 +0900

    A

--- HMERP/src/main/java/com/HMERP/base/controller/CodeController.java
+++ HMERP/src/main/java/com/HMERP/base/controller/CodeController.java
@@ -1,5 +1,36 @@
 package com.HMERP.base.controller;
 
+
+@Controller
 public class CodeController {
-A
+	/* BaseServiceFacade setting */
+	@Autowired
+	private BaseServiceFacade baseServiceFacade;
+	@Autowired
+	private DatasetBeanMapper datasetBeanMapper;
+	
+	@RequestMapping("base/findCodeList.do")
+	public void findCodeList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		PlatformData outData = (PlatformData) request.getAttribute("outData");
+		List<CodeBean> codeList = baseServiceFacade.findCodeList();
+		List<DetailCodeBean> detailCodeList = new ArrayList<DetailCodeBean>();
+		for (CodeBean codeBean : codeList) {
+			List<DetailCodeBean> detailCodeBeanList = codeBean.getDetailCodeList();
+
+			detailCodeList.addAll(detailCodeBeanList);
+		}
+		datasetBeanMapper.beansToDataset(outData, codeList, CodeBean.class);
+		datasetBeanMapper.beansToDataset(outData, detailCodeList, DetailCodeBean.class);
+	}
+	
+	@RequestMapping("base/batchCode.do")
+	public void batchCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		PlatformData inData = (PlatformData) request.getAttribute("inData");
+		CodeInfoBean codeInfoBean = new CodeInfoBean();
+		codeInfoBean.setCodeList(datasetBeanMapper.datasetToBeans(inData, CodeBean.class));
+		codeInfoBean.setDetailCodeList(datasetBeanMapper.datasetToBeans(inData, DetailCodeBean.class));
+		baseServiceFacade.batchCode(codeInfoBean);
+
+		findCodeList(request, response);
+	}
 }
